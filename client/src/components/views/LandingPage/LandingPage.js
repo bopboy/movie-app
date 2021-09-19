3ceb3d3f3049003da59d779f166aa5d5a7cc73d7
bopboy2 <scp0000@gmail.com>
2021-09-20 08:44:59 +0900

    랜딩 페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,59 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config'
+import { Row } from 'antd'
+
+import MainImage from './Sections/MainImage'
+import GridCard from '../commons/GridCard'
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetchMovies(endpoint)
+    }, [])
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint).then(res => res.json()).then(res => {
+            console.log(res.results)
+            setMovies([...Movies, ...res.results])
+            setMainMovieImage(res.results[0])
+            setCurrentPage(res.page)
+        })
+    }
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`
+        fetchMovies(endpoint)
+    }
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movies by Latest</h2>
+                <hr />
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCard
+                                image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={loadMoreItems}>Load More</button>
+            </div>
+        </div >
     )
 }
 
