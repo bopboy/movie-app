588a9041a25eb71beb700478966c7a7f7a7d71ff
bopboy <39391891+bopboy@users.noreply.github.com>
2021-09-20 09:08:02 +0900

    Delete Config.js

--- client/src/components/Config.js
+++
@@ -1,5 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-export const API_URL = 'https://api.themoviedb.org/3/'
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
-export const API_KEY = 'd23b6e209156de66ef98203b83473aac'
